20a73830f7f5ee3332a46669bd4d40ae20dd00a1
Paul Jose <pauljose@live.com>
2022-03-31 20:40:33 +1100

    Added mongodb functionality to application

--- public/js/script.js
+++ public/js/script.js
@@ -2,34 +2,59 @@ const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
 }
 
-$(document).ready(function(){
-    $('.materialboxed').materialbox();
-    $('.modal').modal();
-    addCards(cardList);
-});
+const addProjectToApp = (project) => {
+    $.ajax({
+        url: '/api/projects',
+        data: project,
+        type: 'POST',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
+        }
+    })
+}
 
-const cardList = [{
-        title: "Kuala 2",
-        image: "images/kuala2.jpeg",
-        link: "About Kuala 2",
-        desciption: "Demo desciption about kuala 2"
-    }, {
-        title: "Kuala 3",
-        image: "images/kuala3.jpeg",
-        link: "About Kuala 3",
-        desciption: "Demo desciption about kuala 3"
-    }];
+const submitForm = () => {
+    let formData = {};
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
+
+    addProjectToApp(formData);
+
+    console.log("Form Data Submitted: ", formData);
+}
 
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">'+
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-            '</div><div class="card-content">'+
-            '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-            '<div class="card-reveal">'+
-            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-            '<p class="card-text">'+item.desciption+'</p>'+
-            '</div></div></div>';
-        $("#card-section").append(itemToAppend)
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text grey-text text-darken-4">'+item.description+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
+    });
+}
+
+const getProjects = () => {
+    $.get('/api/projects', (res) => {
+        if (res.statusCode == 200) {
+            addCards(res.data);
+        } else {
+            console.error("Unable to get response from server");
+        }
     });
 };
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    });
+    getProjects();
+    $('.modal').modal();
+  });
--- server.js
+++ server.js
@@ -1,12 +1,78 @@
+require("dotenv").config();
 const express= require("express");
+const cors = require("cors");
 const app= express();
 
-app.use(express.static(__dirname+'/public'));
+const MongoClient = require("mongodb").MongoClient;
+// Connect to the database
+const uri = "mongodb+srv://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASSWORD + "@sit725.pidyf.mongodb.net/SIT725?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+let projectCollection;
+
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended:false}));
+app.use(cors());
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+};
+
+const insertProject = (project, callback) => {
+    projectCollection.insert(project, callback);
+};
+
+const createCollection = (collectionName) => {
+    client.connect((err, db) => {
+        projectCollection = client.db().collection(collectionName);
+        if (!err) {
+            console.log("Successfully created or acquired collection.");
+        } else {
+            console.error("Database error: " + err);
+            process.exit(1);
+        }
+    });
+};
+
+// const cardList = [
+//     {
+//         title: "Kuala 2",
+//         image: "images/kuala2.jpeg",
+//         link: "About Kuala 2",
+//         description: "Demo description about kuala 2"
+//     },
+//     {
+//         title: "Kuala 3",
+//         image: "images/kuala3.jpeg",
+//         link: "About Kuala 3",
+//         description: "Demo description about kuala 3"
+//     }
+// ];
+
+app.get('/api/projects', (req,res) => {
+    getProjects((err, result) => {
+        if (err) {
+            res.json({ statusCode: 400, message: err });
+        } else {
+            res.json({ statusCode: 200, message: "Success", data: result });
+        }
+    });
+});
+
+app.post('/api/projects', (req,res) => {
+    let project = req.body;
+    insertProject(project, (err, result) => {
+        if (err) {
+            res.json({ statusCode: 400, message: err });
+        } else {
+            res.json({ statusCode: 200, message: "Successfully added new project", data: result });
+        }
+    });
+});
 
 const port = 3000;
 
 app.listen(port, ()=> {
-    console.log("App listening to: "+port)
+    console.log("App listening to: "+port);
+    createCollection("pets");
 });
